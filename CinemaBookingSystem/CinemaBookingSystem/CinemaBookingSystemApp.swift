25f51f8ae09b06105b0f502a5a9b47afb75a0eb8
koalneu <126221701+koalneu@users.noreply.github.com>
2024-04-29 16:06:00 +1000

    intiial commit

---
+++ CinemaBookingSystem/CinemaBookingSystem/CinemaBookingSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaBookingSystemApp.swift
+//  CinemaBookingSystem
+//
+//  Created by Mark Jansen Angeles on 29/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct CinemaBookingSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CinemaBookingSystem/CinemaBookingSystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CinemaBookingSystem
+//
+//  Created by Mark Jansen Angeles on 29/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
