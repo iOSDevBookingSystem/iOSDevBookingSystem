1ac50f2ac30d36510b44003e6f1f93156edf1bb5
Tonyworkwasd <168901285+Tonyworkwasd@users.noreply.github.com>
2024-05-12 20:57:04 +1000

    fixed giftcard view

--- CinemaBookingSystem/CinemaBookingSystem/Views/GiftCardsView.swift
+++ CinemaBookingSystem/CinemaBookingSystem/Views/GiftCardsView.swift
@@ -10,22 +10,16 @@ import SwiftUI
 struct GiftCardsView: View {
     @Binding var userAccount: User
 
-
     var body: some View {
-        if userAccount.giftCards.isEmpty {
-            // Display a message when there are no gift cards
-            Text("You don't have any Gift Cards.")
-                .font(.title)
-                .padding()
-                .multilineTextAlignment(.center)
-            
-            NavigationLink(destination: BuyGiftCardView(userAccount: $userAccount)) {
-                Text("Get a New Gift Card")
-            }
-            
-        } else {
-            // List the gift cards when there are some
-            NavigationView {
+        NavigationView {
+            if userAccount.giftCards.isEmpty {
+                // Display a message when there are no gift cards
+                Text("You don't have any Gift Cards.")
+                    .font(.title)
+                    .padding()
+                    .multilineTextAlignment(.center)
+            } else {
+                // List the gift cards when there are some
                 List(userAccount.giftCards, id: \.id) { giftCard in
                     VStack(alignment: .leading) {
                         Text("Balance: $\(giftCard.balance, specifier: "%.2f")")
@@ -33,15 +27,15 @@ struct GiftCardsView: View {
                         Text("Gift Card Code: \(giftCard.id.uuidString)")
                     }
                     .padding(.vertical, 8)
-                    NavigationLink(destination: BuyGiftCardView(userAccount: $userAccount)) {
-                        Text("Get a New Gift Card")
-                    }
                 }
-                }
-                
                 .navigationTitle("My Gift Cards")
             }
         }
+        .navigationBarItems(trailing:
+            NavigationLink(destination: BuyGiftCardView(userAccount: $userAccount)) {
+                Text("Get a New Gift Card")
+            }
+        )
     }
     
     var dateFormatter: DateFormatter {
@@ -49,8 +43,7 @@ struct GiftCardsView: View {
         formatter.dateStyle = .medium
         return formatter
     }
-
-
+}
 //
 //#Preview {
 //    GiftCardsView()
